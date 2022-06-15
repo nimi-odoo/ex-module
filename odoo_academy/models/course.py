86fe0977b0ca9efce991cac93db0c46b7e37f0c8
nimi-odoo <nimi@odoo.com>
2022-06-15 15:43:37 -0400

    [ADD] Added onchange for updating course prices

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
     
@@ -20,3 +21,24 @@ class Course(models.Model):
     )
 
     active = fields.Boolean(string="Active", default=True)
+
+    base_price = fields.Float(string="Base Price", default=0.00)
+    additional_fee = fields.Float(string="Additional Fee", default=10.00)
+    total_price = fields.Float(string="Total Price", readonly=True)
+
+    
+
+    # Set the total price to the base price + additional price when either changes
+    @api.onchange("base_price", "additional_fee")
+    def _onchange_total_price(self):
+        if self.base_price < 0.00:
+            raise UserError("Base price can't be negative")
+        self.total_price = self.base_price + self.additional_fee
+        
+    # Make sure that the additonal fee is atleast $10.00
+    @api.constrains("additional_fee")
+    def _check_additional_fee(self):
+        for record in self: # Verify for all the records
+            if record.additional_fee < 10.00:
+                # raise ValidationError(f"Additional fee must be atleast $10.00, fee is: {record.additional_fee}")
+                raise ValidationError("Additional fee must be atleast $10.00, fee is:")
