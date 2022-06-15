c28f8c1dcfbd7a957106f13ab3781ee5d746f672
nimi-odoo <nimi@odoo.com>
2022-06-15 00:05:37 -0400

    [ADD] Implemented views for odoo academy courses

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -23,7 +23,8 @@
     
     'data': [
         'security/academy_security.xml', # Make sure to load the xml file first. The groups need to be loaded before the access rights
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml'
     ],
     
     'demo': [
