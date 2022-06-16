26bb90938cd3378ff9f60b5c100f387ce26faf23
nimi-odoo <nimi@odoo.com>
2022-06-16 09:36:50 -0400

    [ADD] Used onchange decorator to restrict isbn length to 13 characters

--- library/models/book.py
+++ library/models/book.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class Book(models.Model):
     _name = "library.book"
@@ -20,8 +21,20 @@ class Book(models.Model):
 
     rating = fields.Selection(
         string="Rating", 
-        selection=[('1', '1'), ('2','2'), ('3', '3'), ('4', '4'), ('5', '5')], 
+        selection=[('1', '1 star'), ('2','2 stars'), ('3', '3 stars'), ('4', '4 stars'), ('5', '5 stars')], 
         copy=False
     )
     
     notes = fields.Text(string="Notes")
+
+    @api.onchange("isbn")
+    def _verify_isbn_length(self):
+        without_dashes = "".join(self.isbn.split("-"))
+        if len(without_dashes) != 13:
+            raise ValidationError("The ISBN must be exactly 13 characters long")
+
+    # @api.constrains("isbn")
+    # def _verify_isbn_length(self):
+    #     if not all(len("".join(record.isbn.split("-"))) == 13 for record in self):
+    #         raise ValidationError("The ISBN must be exactly 13 characters long")
+        
