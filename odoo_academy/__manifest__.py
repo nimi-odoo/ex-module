ab18041d01cb066190caa86c1cc17bf3dc40f714
nimi-odoo <nimi@odoo.com>
2022-06-22 16:06:23 -0400

    [ADD] HTML report for sessions

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -29,7 +29,8 @@
         'views/session_views.xml',
         'views/sale_views_inherit.xml',
         'views/product_views_inherit.xml',
-        # 'wizard/sale_wizard_view.xml'
+        # 'wizard/sale_wizard_view.xml',
+        'reports/session_report_templates.xml'
     ],
     
     'demo': [
